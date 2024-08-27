0893109ffd04716660cd06313990dd134e90ba00
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2024-08-27 01:28:11 -0300

    Muda shared na APIManager de let para var

--- Sources/Network/APIManager.swift
+++ Sources/Network/APIManager.swift
@@ -14,7 +14,7 @@ public final class APIManager: ManagerProtocol {
     typealias NetworkResponse = (data: Data, response: URLResponse)
     typealias Failure = APIError
     
-    public static let shared: ManagerProtocol = APIManager()
+    public static var shared: ManagerProtocol = APIManager()
     private init() { }
     
     private let session = URLSession.shared
